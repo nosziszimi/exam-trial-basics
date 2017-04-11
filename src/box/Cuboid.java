97861d4cdfb4d18ef05c4b0273b149e15a05e52f
Aradi Nóra <aradinora111@gmail.com>
2017-04-11 10:44:06 +0200

    Box - create getSurface() method

---
+++ src/box/Cuboid.java
@@ -0,0 +1,19 @@
+package box;
+
+/**
+ * Created by Nóra on 2017. 04. 11..
+ */
+public class Cuboid {
+  int x, y, z;
+
+  public Cuboid(int x, int y, int z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  public int getSurface() {
+    int surface = 2 * (x*y + x*z + z*x);
+    return surface;
+  }
+}
